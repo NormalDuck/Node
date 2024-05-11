3656c57d9ff4ae868f2f42f65c05879b1b57911e
NormalDuck <1juhuang2@gmail.com>
2024-05-10 20:59:33 -0700

    Built and published by Moonwave

---
+++ assets/js/f8b3233d.c59e908b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[789],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Board","label":"\u200bBoard"},{"type":"link","href":"/api/Node","label":"\u200bNode"}]')}}]);
